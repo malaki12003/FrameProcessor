90a2636fce009dc8c322ebfa0c0460ef4db585b6
Mahdi Malaki <malaki12003@gmail.com>
2023-08-19 20:09:25 -0400

    Refactoring

--- source/processor.cpp
+++ source/processor.cpp
@@ -6,43 +6,67 @@
 
 using namespace processor;
 
+// Constructor to initialize the video path
 FrameProcessor::FrameProcessor(std::string _videoPath) : videoPath(std::move(_videoPath)) {}
 
+// Calculate average RGB values for each frame of the video
 std::vector<FrameInfo> FrameProcessor::averageRGB() const {
-  cv::VideoCapture cap(videoPath);
-  if (!cap.isOpened()) {
-    std::cerr << "Error opening video file" << std::endl;
-    return {};
-  }
-
   std::vector<FrameInfo> frameInfoList;
 
-  cv::Mat frame;
-  int frameNumber = 1;
-  while (cap.read(frame)) {
-    cv::Scalar averageColor = cv::mean(frame);
-    FrameInfo frameInfo;
-    frameInfo.rgb[0] = static_cast<int>(averageColor[2]);
-    frameInfo.rgb[1] = static_cast<int>(averageColor[1]);
-    frameInfo.rgb[2] = static_cast<int>(averageColor[0]);
-    frameInfo.frame = frameNumber;
-    frameInfoList.push_back(frameInfo);
-    ++frameNumber;
+  try {
+    // Open the video file
+    cv::VideoCapture cap(videoPath);
+    if (!cap.isOpened()) {
+      std::cerr << "Error opening video file" << std::endl;
+      return frameInfoList;
+    }
+
+    cv::Mat frame;
+    int frameNumber = 1;
+    while (cap.read(frame)) {
+      // Calculate the average color of the frame
+      cv::Scalar averageColor = cv::mean(frame);
+
+      // Create a FrameInfo object to store frame information
+      FrameInfo frameInfo;
+      frameInfo.rgb[0] = static_cast<int>(averageColor[2]);
+      frameInfo.rgb[1] = static_cast<int>(averageColor[1]);
+      frameInfo.rgb[2] = static_cast<int>(averageColor[0]);
+      frameInfo.frame = frameNumber;
+
+      // Add the FrameInfo object to the list
+      frameInfoList.push_back(frameInfo);
+      ++frameNumber;
+    }
+
+    // Release the video capture object
+    cap.release();
+  } catch (const cv::Exception& e) {
+    std::cerr << "OpenCV Exception: " << e.what() << std::endl;
+  } catch (const std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
   }
 
-  cap.release();
   return frameInfoList;
 }
 
+// Convert a list of FrameInfo objects to JSONL format
 std::string FrameProcessor::frameInfoListToJsonL(
     const std::vector<FrameInfo>& frameInfoList) const {
   std::string jsonLines;
-  for (const FrameInfo& frameInfo : frameInfoList) {
-    std::string jsonLine = "{\"frame\": " + std::to_string(frameInfo.frame) + ", \"rgb\": ["
-                           + std::to_string(frameInfo.rgb[0]) + ", "
-                           + std::to_string(frameInfo.rgb[1]) + ", "
-                           + std::to_string(frameInfo.rgb[2]) + "]}";
-    jsonLines += jsonLine + '\n';
+
+  try {
+    for (const FrameInfo& frameInfo : frameInfoList) {
+      // Create a JSON line for each FrameInfo object
+      std::string jsonLine = fmt::format(R"({{"frame": {}, "rgb": [{}, {}, {}]}})", frameInfo.frame,
+                                         frameInfo.rgb[0], frameInfo.rgb[1], frameInfo.rgb[2]);
+
+      // Add the JSON line to the accumulated JSONL output
+      jsonLines += jsonLine + '\n';
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
   }
+
   return jsonLines;
 }
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -5,9 +5,11 @@
 
 #include <cxxopts.hpp>
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <unordered_map>
+#include <vector>
 
+// Structure to hold parsed RabbitMQ URI information
 struct RmqUriInfo {
   std::string username;
   std::string password;
@@ -16,6 +18,7 @@ struct RmqUriInfo {
   std::string exchangeName;
 };
 
+// Parse the provided RMQ URI
 bool parseRmqUri(const std::string& rmqUri, RmqUriInfo& rmqInfo) {
   // Check for the correct URI scheme
   if (rmqUri.find("amqp://") != 0) {
@@ -35,69 +38,81 @@ bool parseRmqUri(const std::string& rmqUri, RmqUriInfo& rmqInfo) {
   return true;
 }
 
-auto main(int argc, char** argv) -> int {
-  cxxopts::Options options(*argv, "A program to welcome the world!");
+int main(int argc, char** argv) {
+  // Set up command-line options
+  cxxopts::Options options(*argv, "A program to process video frames and send data to RMQ.");
 
   std::string name;
   std::string rmqUri;
 
-  // clang-format off
-  options.add_options()
-    ("r,rmq", "RMQ URI", cxxopts::value(rmqUri)->default_value(""))
-    ("v,video", "Video path", cxxopts::value(name)->default_value(""));
-  // clang-format on
+  options.add_options()("r,rmq", "RMQ URI", cxxopts::value(rmqUri)->default_value(""))(
+      "v,video", "Video path", cxxopts::value(name)->default_value(""));
 
   auto result = options.parse(argc, argv);
 
+  // Create a FrameProcessor instance to process video frames
   processor::FrameProcessor processor(name);
-  std::vector<processor::FrameInfo> frameInfoList = processor.averageRGB();
-  std::string jsonlOutput = processor.frameInfoListToJsonL(frameInfoList);
-  if (rmqUri.length() > 0) {
-    RmqUriInfo rmqInfo;
 
-    if (parseRmqUri(rmqUri, rmqInfo)) {
-      std::cout << "Username: " << rmqInfo.username << std::endl;
-      std::cout << "Password: " << rmqInfo.password << std::endl;
-      std::cout << "Host: " << rmqInfo.host << std::endl;
-      std::cout << "Port: " << rmqInfo.port << std::endl;
-      std::cout << "Exchange Name: " << rmqInfo.exchangeName << std::endl;
+  try {
+    // Calculate average RGB values for each frame
+    std::vector<processor::FrameInfo> frameInfoList = processor.averageRGB();
+
+    // Generate JSONL output from frame information
+    std::string jsonlOutput = processor.frameInfoListToJsonL(frameInfoList);
+
+    if (rmqUri.length() > 0) {
+      RmqUriInfo rmqInfo;
+
+      if (parseRmqUri(rmqUri, rmqInfo)) {
+        // Establish a connection to RabbitMQ server
+        amqp_connection_state_t conn = amqp_new_connection();
+        amqp_socket_t* socket = amqp_tcp_socket_new(conn);
+
+        if (!socket) {
+          std::cerr << "Error creating TCP socket" << std::endl;
+          return 1;
+        }
+
+        // Open a socket to the RabbitMQ server
+        int status = amqp_socket_open(socket, rmqInfo.host.c_str(), rmqInfo.port);
+        if (status != AMQP_STATUS_OK) {
+          std::cerr << "Error opening socket" << std::endl;
+          return 1;
+        }
+
+        // Perform authentication with the RabbitMQ server
+        amqp_rpc_reply_t reply = amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
+                                            rmqInfo.username.c_str(), rmqInfo.password.c_str());
+        if (reply.reply_type != AMQP_RESPONSE_NORMAL) {
+          std::cerr << "Login failed." << std::endl;
+          return 1;
+        }
+
+        // Open a channel and get RPC reply
+        amqp_channel_open(conn, 1);
+        amqp_get_rpc_reply(conn);
+
+        // Publish JSON message to RabbitMQ
+        amqp_bytes_t message_bytes = amqp_cstring_bytes(jsonlOutput.c_str());
+        amqp_basic_publish(conn, 1, amqp_cstring_bytes(rmqInfo.exchangeName.c_str()),
+                           amqp_cstring_bytes(""), 0, 0, nullptr, message_bytes);
+
+        // Close the channel and connection
+        amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
+        amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
+        amqp_destroy_connection(conn);
+
+        std::cerr << "JSON message sent to RabbitMQ." << std::endl;
+      } else {
+        std::cerr << "Invalid RMQ URI format" << std::endl;
+      }
     } else {
-      std::cout << "Invalid RMQ URI format" << std::endl;
+      // Print JSONL output if RMQ URI is not provided
+      std::cout << jsonlOutput;
     }
-    amqp_connection_state_t conn = amqp_new_connection();
-    amqp_socket_t* socket = amqp_tcp_socket_new(conn);
-
-    if (!socket) {
-      std::cerr << "Error creating TCP socket" << std::endl;
-      return 1;
-    }
-
-    int status = amqp_socket_open(socket, rmqInfo.host.c_str(), rmqInfo.port);
-    if (status != AMQP_STATUS_OK) {
-      std::cerr << "Error opening socket" << std::endl;
-      return 1;
-    }
-    amqp_rpc_reply_t reply = amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
-                                        rmqInfo.username.c_str(), rmqInfo.password.c_str());
-    if (reply.reply_type != AMQP_RESPONSE_NORMAL) {
-      std::cerr << "Login failed." << std::endl;
-      return 1;
-    }
-    std::cerr << "Login succ!" << std::endl;
-    amqp_channel_open(conn, 1);
-    amqp_get_rpc_reply(conn);
-
-    // Publish JSON message to RabbitMQ
-    amqp_bytes_t message_bytes = amqp_cstring_bytes(jsonlOutput.c_str());
-    amqp_basic_publish(conn, 1, amqp_cstring_bytes(rmqInfo.exchangeName.c_str()),
-                       amqp_cstring_bytes(""), 0, 0, nullptr, message_bytes);
-
-    amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
-    amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
-    amqp_destroy_connection(conn);
-
-  } else {
-    std::cout << jsonlOutput;
+  } catch (const std::exception& e) {
+    std::cerr << "An error occurred: " << e.what() << std::endl;
+    return 1;
   }
 
   return 0;
